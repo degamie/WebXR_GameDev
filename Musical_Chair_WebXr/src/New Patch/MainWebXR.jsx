116e485ce06ca0d3900eae3e9aa0c4db0c6a882b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-16 16:08:08 +0530

    Add files via upload

--- Musical_Chair_WebXr/src/New Patch/MainWebXR.jsx
+++ Musical_Chair_WebXr/src/New Patch/MainWebXR.jsx
@@ -1,8 +1,12 @@
-//WID(14/11/2025)(By Sarthak Mittal(DegamieSign))#1.1
+//WID;//Impl(16/11/2025)(By Sarthak Mittal(DegamieSign))#1.1
 const setScene(scene){this.scene;}//Binding Scene
 const scene=new BABYLON.scene(engine);
 const getScene(scene){return scene;}
 const setScene(scene){this.scene=scene;}
+
+const getLight=(Light)=>{return Lightl;}
+const setLight=(Light)=>{this.Light=Light;}//Binding  Light
+
 BABYLON.PostProcess=function(name,fragmentUrl,Parameters,samplers,ratio,camera,samplingMode,engine,resuable);
 const xrCam=new WebXRCamera("Camera1",scene,xrSessionManager);
 const getxrCam=(xrCam)=>{return xrCam;}//Fetching XRCamera
@@ -22,13 +26,15 @@ const setDirectionZ=(directionZ)=>{this.directionZ=directionZ;}//binding  Direct
 const getDirectionZ=(directionZ)=>{return directionZ;}//Fetching DirectionZ
 const direction_opp_Z=xrCam.getDirection(-(Axis.Z))+"Camera Direction On Back";
 const Height=xrCam.realWorldHeight;
+const getHeight=(Height)=>{return Height;}//Fetching height in app
+const setHeight=(Height)=>{this.Height=Height;}//Binding Height in Apps
 xrCam.setTransformationFromNonVRCamera(xrCam,true);
 xrCam.getTransformationFromNonVRCamera(xrCam,true);//Fetching Non-VR Canera's Transformation
 const Width=xrCam.realWorldWidth;
 const getWidth=(width)=>{return width;}//printing Width
 const setWidth=(width)=>{return width;}//Binding Width in It
 const Light=new BABYLON.PointLight("pointLight",new BABYLON.Vector3(1,10,5),scene);//impl(28/09/2025)
-const getLight=(Light)=>{return Lightl;}
+
 const shape=new BABYLON.Cylinder(
 new BABYLON.Vector3(0,-.9,0),
 new BABYLON.Vector3(0,+.9,0),
