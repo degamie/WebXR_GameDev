991ee4aad20c18caaa507f41029eca4a20013d82
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-02 11:44:25 +0530

    Add files via upload

--- Musical_Chair_WebXr/src/components/App.jsx
+++ Musical_Chair_WebXr/src/components/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-///WID(30/11/2025)#1.1
+///WID(2/12/2025)#1.1
 //(WebXR:) ThreeJs(Fibre)
 function setupScene({scene,camera,renderer,player,controller}){
     const getPlayerPos=(Player,x,y,z)=>{return Player+x+y+z;}//Fetching PllayerPos in Server
@@ -23,6 +23,7 @@ function setupScene({scene,camera,renderer,player,controller}){
     camera.add(listener);
     camera.update(listener);
     listener.add(ScoreSnd);
+    camera.exists(listener);//Checking Camera's Listener
     const scoreSnd=new THREE.PositionalAudio(listnener);//Adding ScoreBoardSound
     audioLoader.load("assets/score.mp3",buffer=>{
         scoreSnd.setBuffer(buffer);scoreTxt.add(scoreSnd);});//loading ScoreBoardSound
