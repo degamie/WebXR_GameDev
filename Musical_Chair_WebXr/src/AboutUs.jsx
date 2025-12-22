a1684f63d2eaa16525c0bb99188b8c472e7f23d5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-22 08:41:15 +0530

    Add files via upload

---
+++ Musical_Chair_WebXr/src/AboutUs.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+//WID(18/12/2025)//Sarthak Mittal(Degamiesign)#1
+function AboutUs() {
+    AboutUs(AuthorName,window.innerWidth,window.innerHeight){//InnerWidth and innerHeight's Declare
+        this.AuthorName=AuthorName;
+        this.innerWidth=innerWidth;//Binding InnerWidth
+        this.innerHeight=innerHeight;//InnerWidth and innerHeight's Binding
+        }
+  return (
+      setinnerWidth(InnerWidth){this.InnerWidth=InnerWidth;}
+      getinnerWidth(InnerWidth){return InnerWidth;}
+      getAuthorId=(AuthorId)={return AuthorId;}//Fetching AuthorId in Server
+      setAuthorId=(AuthorId)={this.AuthorId=AuthorId;}//Binding AuthorId in Servers
+      updateAllByAuthorId=(AuthorId)=>{getAuthorId(AuthorId)+setAuthorId(AuthorId)+1;}//Updating AuthorId in App
+       updateByinnerWidth=(InnerWidth)=>{getinnerWidth(innerWidth)+setinnerWidth(innerWidth)+1;}//Updating Inner Width in It
+       updateByinnerHeight=(InnerHeight)=>{getinnerHeight(innerHeight)+setinnerHeight(innerHeight)1;}//updating Inner height inn App
+      setAuthorName(AuthorName){this.AuthorName=AuthorName;}
+      getAuthorName(AuthorName){return AuthorName;}
+      updateAllByAuthorName=(AuthorName)=>{getAuthorName(AuthorName)+setAuthorName(AuthorName)+1;}//Udpating All Author's Name in Server
+      setProductsDataToShow(ProductData.length){this.ProductData=ProductData;}
+    getProductsToShow=(ProductsData.length)=>{
+      return ProductsData+1;
+    }
+updateAllByProductsToShow=(ProductData.length)=>{
+    getProductsToShow(ProductData.length)+setProductsDataToShow(ProductData.length)+1;
+    }
+const existsByAuthorName=(authorName)={if(authorName!==null)getAuthorName(authorName);else getAuthorName(authorName);}//Checking AuthorName in App
+
+setOuterWidth(outerWidth.length){this.outerWidth=outerWidth;}
+getOuterHeight(outerHeight.length){return outerHeight;}//Fetching OuterHeight in Server
+setOuterHeight(outerHeight.length){this.outerHeight=outerHeight;}
+getOuterWidth(outerWidth.length){
+    return outerWidth;}
+    setOuterWidth=(outerWidth.length){this.outerWidth=outerWidth;}//Binding OuterWidth in Server
+    const updateByouterWidth=(outerWidth)=>{getOuterWidth(outerWidth)+setOuterWidth(outerWidth)+1;}//Updating Outer Width in Server
+      setCardsToShow(ProductsData.length){
+          this.ProductsData.length=ProductsData.length;}
+          getCardSToShow=(ProductData.length)=>{return ProductsData.length; }//Fetching ProductData in Server
+                 getinnerWidth(window.innerWidth){
+                                   return innerWidth+1;}
+                                   setinnerWidth(window.innerWidth){
+                                       this.innerWidth=innerWidth;  }
+                                   const updateCardsToShow=(ProductData.length)=>{getCardSToShow(ProductData)+setCardsToShow(ProductData+1;}//Updating Cards To Show
+     <div>
+      <div>
+          getinnerHeight(window.innerHeight){
+              return innerHeight;}
+
+          const useEffect=(()=>{
+          const updateCardsToShow=()=>{
+            if(window.innerWidth>=1024){
+              setCardsToShow(ProductsData.length);
+            }
+              else setCardsToShow(1);
+              window.addEventListener();   
+          }
+
+        });
+    return ()=>{
+      window.removeEventListner('resize',updateCardsToShow);
+       window.addEventListener("resize",updateCardsToToShow());
+    }       
+  <div> AboutUs
+    <h2>Welcome,We Provide High End Business Solutions Globally </h2>
+    <div className='flex flex-col items-center container mx-auto p-14 md:px-20 lg:px-35 w-full overflow-display 'id='About Us'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'></h1>
+              <p className ='text-5xl font-medium text-blue-white-800'>Believe In!</p>
+              <div className='flex gap-8 transition-transform duration-500 ease-in-out 'style={{transform:'translateX'('$(currentIndx*100/cardsTodisplaty'%)}}
+             
+//         <div>
+//           <div>
+//             {ProductData.map((Product,index)=>(
+//               <div key={index} className='relative flex -shrink-0 w-full sm:w-1/4'>
+//                 <img src={ProductImg} alt={Product.img}>
+//                 </img>
+//                 </div>
+//             ))}
+//           </div>
+//         </div>
+//        <h2> Mobile App Dev
+//         <div>Design,Build and Develop Most PowerFull App Dev Integrably</div></h2>
+//         <div className='grid grid-cols-2 gap-8 md:gap-4 w-full 2xl:pr-28'>
+
+        <p className ='text-5xl font-medium text-yellow-white-800'>Hiring</p>
+        </div>
+        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'></div>
+        <img src={assets}alt=""></img>
+    </div>
+  
+      </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default AboutUs
