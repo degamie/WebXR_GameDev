efb86ede25769abd85d87f5a66b33b90d307925a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-26 20:46:48 +0530

    Add files via upload

---
+++ Musical_Chair_WebXr/src/New Patch/ContactUs.jsx
@@ -0,0 +1,75 @@
+//WID(26/1/2026)//Sarthak mittal a.k.a(Degamiesign)#11.1.1.1.1.1
+import React from 'react'
+message=0;
+
+function ContactUs() {
+  ContactUs(message){
+      this.userId=userId;
+      this.email=email;
+    this.message=this.message;
+    this.userName=userName;
+  }
+
+  return (
+      const getId=(id)=>{return id;}//Fetching Id In App
+      const setId=(id)=>{this.id=id;}//Bidning Id in App
+      const getMessage(message){
+          return message;}
+          const getContactus=(ContactUs )=>{return ContactUs;}//Fetching ContactUs in App
+          const setMessage=(message)=>{this.message=message;}//Binding Message in Seever
+          const setMessage(message){this.message=message;}
+          const getAllByUSerName=(UserName)=>{return UserName;}//Fetching UserName In App
+          const updateAllByMessage=(message)=>{getMessage(message)+setMessage(message)+1;}//Updating Message in app
+          const getAllByEmail=(email)=>{return email;}//Fetching Email in Server
+          const setAllByEmail=(email)=>{this.email=email;}//Binding Email in Server
+          const updateAllByEmail=(email)=>{getAllByEmail(email)+setAllByEmail(email)+1;}//Updating Emails in Server
+          const getPhoneNum(PhoneNumber){return PhoneNumber;}
+          const setPhoneNUm(PhoneNumber){this.PhoneNumber=PhoneNumber;}
+          const getAllByPhoneNumber=(PhoneNumber)=>{return PhoneNumber;}//Fetching PhoneNumber in App 1
+          const updateAllByPhoneNum=(PhoneNumber)=>{getPhoneNum(PhoneNumber)+setPhoneNUm(PhoneNumber);}//Updating All PhoneNumber
+            const getAllByPassword=(Password)=>{return Password;}//Fetchig Password in Server
+        const updateAllByPassword=(Password)=>{getAllByPassword(Password)+setAllByPassword(Password)+1;}//updating Password in Server     //password get nd setter //TBC
+        const setAllBypasssword=(Passowrd)=>{this.Password=Password;}//Binding password in App
+        const existsByPassword=(password)=>{if(password.length!=0)getAllByPassword(password);else getAllByPassword(0);}//checking Passowrd's Length in APp
+
+    <div>
+      <form className='max-w-2xl mx-auto text-blue -550 pt-8'>
+     <div className='w-full md:w-1/2 text-left'>Name 
+      <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="Name" placeholder='Your FullName Required'>
+      </input>
+            <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="Email" placeholder='Your Email Required'></input>
+                  <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="PhoneNumber" placeholder='Your PhoneNumber Required'></input>
+     <div>Message<textArea name="Message" placeholder='Message'></textArea></div>
+{/*      Update(9/10/2025): */}
+     <div>{Array.from({length:jobs.count.rating,item,index)},=>(
+     <img key={index}, src=" " , alt=" ">
+     //      10/08/2025 Update
+     <p className='text-blue-680'>{jobs.text}</p>
+     if(data.success){
+         setResult("Form Submitted Successfully");
+         event.target.reset();}
+         else console.log("Error Message Displaying",data);
+         alert(data.message)
+         setResult("");
+{/*      <div> */}
+
+     </div>
+     </div>
+
+    </form>
+  
+        <div className='my-6 text-left'>
+          <button className='bg-blue-650 text-purple py-2 px-12 py-2 px-12 py-5 mb-10 rounded'>Send Message</button>
+    <div className='Container mx-auto py-4 pt-16 px-7 md:px-30 lg:px-32 my-20 w-full overflow-hidden'id='Contact Us'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>ContactUs </h1>
+        <p className='text-center text-purple-500  mb-8 max-w-80 mx-auto'></p>
+        <p className='text-center text-orange-500  mb-8 max-w-85 mx-auto'>Reach Us!</p>
+ 
+
+       </div>
+     </div> </div>
+
+  )
+}
+
+export default ContactUs
