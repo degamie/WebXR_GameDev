e945d60210eb3b9feb320898b13e3d2c5ec65938
Degamie_sign <degamiesign@gmail.com>
2026-02-14 12:18:02 +0530

    Seventh Commit

--- Musical_Chair_WebXr/src/App.jsx
+++ Musical_Chair_WebXr/src/App.jsx
@@ -1,9 +1,10 @@
-///WID(13/2/2026)#Degamiesign(Sarthak Mittal)1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
+///WID(14/2/2026)#Degamiesign(Sarthak Mittal)1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 App app=new App();
+getApp(App app){return app;}
 setApp(App app){this.app=app;}//Binding App in WeBXR
 App(player,renderer){
     this.renderer=renderer;
